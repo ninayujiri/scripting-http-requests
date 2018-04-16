99fe31cbd4b0c6ac1a1c47f866c979a83e343da2
Nina Yujiri <ninayujiri@gmail.com>
2018-04-16 18:40:44 +0000

    added uppercase function

---
+++ callbacks/print_uppercase.js
@@ -0,0 +1,13 @@
+var getHTML = require('../http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/uppercase.html'
+};
+
+function printUpperCase (html) {
+  var upperCase = html.toUpperCase();
+  console.log(upperCase);
+};
+
+getHTML(requestOptions, printUpperCase);
