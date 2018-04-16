dcf63ab3dd521ad9b4fe3fe467b89f96611a4aa8
Nina Yujiri <ninayujiri@gmail.com>
2018-04-16 16:36:31 +0000

    added buffered function

--- chunks.js
+++ chunks.js
@@ -5,17 +5,18 @@ function getAndPrintHTMLChunks () {
     host: 'sytantris.github.io',
     path: '/http-examples/step1.html'
   };
+
   https.get(requestOptions, function (response) {
-  response.setEncoding('utf8');
-  response.on('data', function (data) {
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
     console.log('Chunk Received. Length:', data.length);
-  });
-  response.on('end', function() {
+    });
+
+    response.on('end', function() {
     console.log('Response stream complete.');
-  });
+    });
   });
 }
-
-
 
 getAndPrintHTMLChunks();
